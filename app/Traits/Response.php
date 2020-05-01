a37063a4add925f2a2b7df46abfe4292037334ac
ajibukunoluwa <ibukunajmoti@gmail.com>
2020-05-01 11:07:54 +0100

    complete implementation of seeders

--- app/Traits/Response.php
+++ app/Traits/Response.php
@@ -13,25 +13,4 @@ trait Response
         ], $statusCode);
     }
 
-    public function customPagination(string $table, object $paginated)
-    {
-        $data = $paginated->toArray();
-
-        return collect([
-            $table   => $data['data'],
-
-            'pagination' => [
-                'to'        => $data['to'],
-                'from'      => $data['from'],
-                'total'     => $data['total'],
-                'per_page'  => $data['per_page'],
-                'last_page' => $data['last_page'],
-                'current_page'  => $data['current_page'],
-                'last_page_url' => $data['last_page_url'],
-                'next_page_url' => $data['next_page_url'],
-                'prev_page_url' => $data['prev_page_url'],
-                'first_page_url' => $data['first_page_url'],
-            ]
-        ]);
-    }
 }
